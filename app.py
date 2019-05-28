3a0733b095ab9f6324b1d6d191506157ee15d314
PIKVICWORK\pikvic <truepikvic@gmail.com>
2019-05-28 15:41:15 +1000

    Added count.html and route for /count with GET/POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,10 @@ crows_count = 100500
 @app.route('/')
 def index():
     return render_template('index.html', crows_count=crows_count)
+
+@app.route('/count', methods=['GET', 'POST'])
+def count():
+    global crows_count
+    if request.method == 'POST':
+        crows_count+=1
+    return render_template('count.html', crows_count=crows_count)
