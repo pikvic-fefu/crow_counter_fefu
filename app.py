f09d6e4be38070523188cb5586634c2bdb83fc5d
Alexey Zagumennov <truepikvic@gmail.com>
2019-05-30 00:24:04 +1000

    Added SocketIO button and logic

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+app.config['SECRET_KEY'] = 'secret!'
 
 crows_count = 100500
 
@@ -20,3 +24,10 @@ def add_crow():
     global crows_count
     crows_count += 1
     return jsonify({'crows_count': crows_count})
+
+@socketio.on("add crow")
+def add():
+    global crows_count
+    crows_count += 1
+    print("crow added")
+    emit('crow added', {'crows_count': crows_count}, broadcast=True)   
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    document.querySelector('#ajax').onclick = () => {
+    // Connect to websocket
+    var socket = io(location.protocol + '//' + document.domain + ':' + location.port);
+    
+    socket.on('connect', function(){
+        console.log('connected');
+    });
+
+    socket.on('crow added', function(data){
+        console.log('crow added')
+        document.querySelector('#crows_count').innerHTML = data.crows_count
+    });
+
+    socket.on('disconnect', function(){
+        console.log('disconnected');
+    });
+
+    document.querySelector('#socket').onclick = () => {
+        socket.emit('add crow');
+    };
+    
+
+   document.querySelector('#ajax').onclick = () => {
 
         // Создаём объект AJAX запроса
 
@@ -22,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             document.querySelector('#crows_count').innerHTML = data.crows_count;
             
-        }
+        };
 
         // Посылаем AJAX запрос
 
