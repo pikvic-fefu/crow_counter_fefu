f7902e22f1f27f6b9c779799691edc8766e6e331
Alexey Zagumennov <truepikvic@gmail.com>
2019-06-05 23:03:04 +1000

    Add Bootstrap and nice layout

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
+import sqlite3
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'secret'
 
 crows_count = 100500
 
@@ -17,7 +18,7 @@ def count():
     global crows_count
     if request.method == 'POST':
         crows_count += 1
-    return render_template('count.html', crows_count=crows_count)
+    return render_template('count.html', crows_count=crows_count, users=crows_by_user)
 
 @app.route('/add_crow', methods=['POST'])
 def add_crow():
@@ -29,5 +30,4 @@ def add_crow():
 def add():
     global crows_count
     crows_count += 1
-    print("crow added")
-    emit('crow added', {'crows_count': crows_count}, broadcast=True)   
+    emit('crow added', {'crows_count': crows_count}, broadcast=True)
